3b03c3f7f18a5ceab37ea7ec75552a3758d18704
Carl Fürstenberg <carl.furstenberg@purplescout.se>
2021-02-17 23:52:42 +0100

    fix duplicate image names

--- commandThread.js
+++ commandThread.js
@@ -9,6 +9,8 @@ const download = require("download");
 const chalk = require("chalk");
 const Spinners = require("spinnies");
 const { promisify } = require("util");
+const { nanoid } = require("nanoid");
+const { parse } = require("path");
 const writeFile = promisify(fs.writeFile);
 
 let spinners;
@@ -38,35 +40,52 @@ const commandThread = async (url, outdir, options) => {
     spinners.succeed(meta);
 
     await Promise.all(
-      post.post.images.map((image) => {
-        return new Promise((resolve, reject) => {
-          const target = path.join(
-            outdir,
-            post.poster,
-            post.post.date,
-            image.txt
-          );
-          spinners.add(target, { text: target });
-          if (fs.existsSync(target)) {
-            spinners.succeed(target, {
-              text: `skipped ${target}`,
-              succeedColor: "yellow",
-            });
-            resolve();
-          } else {
-            download(image.src)
-              .on("end", () => {
-                spinners.succeed(target);
-                resolve();
-              })
-              .on("fail", () => {
-                spinners.fail(target);
-                reject();
-              })
-              .pipe(fs.createWriteStream(target));
+      post.post.images
+        .reduce((acc, cur, idx, arr) => {
+          let i = 0;
+          const {name, ext} = parse(cur.txt);
+          while (
+            acc.some((o) => o.txt === (i === 0 ? cur.txt : `${name} (${i})${ext}`))
+          ) {
+            i++;
           }
-        });
-      })
+
+          if (i !== 0) {
+            cur.txt = `${name} (${i})${ext}`;
+          }
+          acc.push(cur);
+          return acc;
+        }, [])
+        .map((image) => {
+          const id = nanoid();
+          return new Promise((resolve, reject) => {
+            const target = path.join(
+              outdir,
+              post.poster,
+              post.post.date,
+              image.txt
+            );
+            spinners.add(id, { text: target });
+            if (fs.existsSync(target)) {
+              spinners.succeed(id, {
+                text: `skipped ${target}`,
+                succeedColor: "yellow",
+              });
+              resolve();
+            } else {
+              download(image.src)
+                .on("end", () => {
+                  spinners.succeed(id);
+                  resolve();
+                })
+                .on("fail", () => {
+                  spinners.fail(id);
+                  reject();
+                })
+                .pipe(fs.createWriteStream(target));
+            }
+          });
+        })
     );
   }
 };
